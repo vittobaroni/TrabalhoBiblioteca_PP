d145efd224d41f5f4fb0ee82f73b77d34dd7e9fa
Eduardo Almeida <eduardo.hevangelista@gmail.com>
2025-01-15 04:12:38 -0300

    at.Aqruivos

--- Sistema/Principal.java
+++ Sistema/Principal.java
@@ -1,11 +1,5 @@
 import java.time.LocalDate;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 
 public class Principal {
 
